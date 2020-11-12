baea793b4f88ce53c99fd803fbbe46ec8fa459aa
jojo13572001 <jojo13572001@yahoo.com.tw>
2020-11-13 00:57:44 +0800

    add robot arm urdf

--- runTraining.sh
+++ runTraining.sh
@@ -1,10 +1,8 @@
 #/bin/bash
+docker image build -t mpc:1.0 .
 docker rm -f mpcTraining
-docker run -i -v C:/Users/Administrator/Documents/git/MPC-Net/pick_place_mpc_app:/pick_place_mpc_app --workdir /pick_place_mpc_app -p 1234:1234 --name=mpcTraining mpc:1.0 bash run.sh
-#cd pick_place_mpc_app
-#./pick_place_mpc_app --port=1234 --server --config=elfin3.info --wait 5 &
-#cd ..
+docker run -i -v C:/Users/Administrator/Documents/git/MPC-Net/pick_place_mpc_app:/pick_place_mpc_app --workdir /pick_place_mpc_app -p 1234:1234 --name=mpcTraining mpc:1.0 bash run.sh &
 sleep 1
-python pybulletArm.py &
+python3 pybulletArm.py &
 sleep 1
-python arm_learner.py
+python3 arm_learner.py
