6a6be8ae2d7b56f4c262a58039e992a51d3ea8d0
Bean <bean.yen@coretronic.com>
2020-11-26 15:43:02 +0800

    add empty folder

--- settings.py
+++ settings.py
@@ -8,4 +8,4 @@ loadPolicyPath = 'armPolicy/pyBullet/1115/mpcPolicy_2020-11-15_225649.pt'
 loadMemoryPath = "armPolicy/pyBullet/1115/mpcPolicy_2020-11-15_233515_memory.pkl"
 
 learning_iterations = 100000
-save_path = "armPolicy/pyBullet/1124/mpcPolicy_"
+save_path = "armPolicy/pyBullet/mpcPolicy_"
